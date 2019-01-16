e935916a96a327587a8c0c33ae6f6f7fe3951220
Venktesh Kaviarasan <43897540+Venkteshkavi@users.noreply.github.com>
2019-01-16 11:43:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,354 @@
+
+# coding: utf-8
+
+# In[259]:
+
+
+from sklearn.cluster import KMeans
+import numpy as np
+import csv
+import math
+import matplotlib.pyplot
+from matplotlib import pyplot as plt
+
+
+# In[260]:
+
+
+maxAcc = 0.0
+maxIter = 0
+C_Lambda = 0.03
+TrainingPercent = 80
+ValidationPercent = 10
+TestPercent = 10
+M = 10
+PHI = []
+IsSynthetic = False
+
+
+# In[261]:
+
+
+# GetTarget Vector Function is used for fetching the data from the Target Vector File where the target vector values
+# range from (0,1,2)
+def GetTargetVector(filePath):
+    t = []
+    with open(filePath, 'rU') as f:
+        reader = csv.reader(f)
+        for row in reader:  
+            t.append(int(row[0]))
+    #print("Raw Training Generated..")
+    return t
+
+# GenerateRawData Fucntion is used to fetch the data from the input data file which is query url pair file
+def GenerateRawData(filePath, IsSynthetic):    
+    dataMatrix = [] 
+    with open(filePath, 'rU') as fi:
+        reader = csv.reader(fi)
+        for row in reader:
+            dataRow = []
+            for column in row:
+                dataRow.append(float(column))
+            dataMatrix.append(dataRow)
+    
+    if IsSynthetic == False :
+        dataMatrix = np.delete(dataMatrix, [5,6,7,8,9], axis=1)
+    dataMatrix = np.transpose(dataMatrix)     
+    #print ("Data Matrix Generated..")
+    return dataMatrix
+
+# In GenerateTrainingTarget function we consider only 80% of the target labels for traning the data
+def GenerateTrainingTarget(rawTraining,TrainingPercent = 80):
+    TrainingLen = int(math.ceil(len(rawTraining)*(TrainingPercent*0.01)))
+    t           = rawTraining[:TrainingLen]
+    #print(str(TrainingPercent) + "% Training Target Generated..")
+    return t
+
+
+# Similarly in GenerateDataMatrix we consider only 80% of the data matrix for training 
+def GenerateTrainingDataMatrix(rawData, TrainingPercent = 80):
+    T_len = int(math.ceil(len(rawData[0])*0.01*TrainingPercent))
+    d2 = rawData[:,0:T_len]
+    #print(str(TrainingPercent) + "% Training Data Generated..")
+    return d2
+
+# Here 1% of the data matrix is used for validation
+def GenerateValData(rawData, ValPercent, TrainingCount): 
+    valSize = int(math.ceil(len(rawData[0])*ValPercent*0.01))
+    V_End = TrainingCount + valSize
+    dataMatrix = rawData[:,TrainingCount+1:V_End]
+    #print (str(ValPercent) + "% Val Data Generated..")  
+    return dataMatrix
+
+# In this function 1% of the target labels are used for validation
+def GenerateValTargetVector(rawData, ValPercent, TrainingCount): 
+    valSize = int(math.ceil(len(rawData)*ValPercent*0.01))
+    V_End = TrainingCount + valSize
+    t =rawData[TrainingCount+1:V_End]
+    #print (str(ValPercent) + "% Val Target Data Generated..")
+    return t
+
+# Big sigma is the co-variance matrix which we generate to find the basis function
+def GenerateBigSigma(Data, MuMatrix,TrainingPercent,IsSynthetic):
+    BigSigma    = np.zeros((len(Data),len(Data)))
+    DataT       = np.transpose(Data)
+    TrainingLen = math.ceil(len(DataT)*(TrainingPercent*0.01))        
+    varVect     = []
+    for i in range(0,len(DataT[0])):
+        vct = []
+        for j in range(0,int(TrainingLen)):
+            vct.append(Data[i][j])    
+        varVect.append(np.var(vct))
+    
+    for j in range(len(Data)):
+        BigSigma[j][j] = varVect[j]
+    if IsSynthetic == True:
+        BigSigma = np.dot(3,BigSigma)
+    else:
+        BigSigma = np.dot(200,BigSigma)
+    ##print ("BigSigma Generated..")
+    return BigSigma
+
+# In this function we are subtracting the input query value from the mean and multiplying it with the transpose
+# of the subtacted value and the hence generated output is multiplied again with the subtracted value.
+def GetScalar(DataRow,MuRow, BigSigInv):  
+    R = np.subtract(DataRow,MuRow)
+    T = np.dot(BigSigInv,np.transpose(R))  
+    L = np.dot(R,T)
+    return L
+
+# Using th formula of gaussian radial basis function we calculate the phi value
+def GetRadialBasisOut(DataRow,MuRow, BigSigInv):    
+    phi_x = math.exp(-0.5*GetScalar(DataRow,MuRow,BigSigInv))
+    return phi_x
+
+# In this function we are deriving the design matrix from the above generated values
+def GetPhiMatrix(Data, MuMatrix, BigSigma, TrainingPercent = 80):
+    DataT = np.transpose(Data)
+    TrainingLen = math.ceil(len(DataT)*(TrainingPercent*0.01))         
+    PHI = np.zeros((int(TrainingLen),len(MuMatrix))) 
+    BigSigInv = np.linalg.inv(BigSigma)
+    for  C in range(0,len(MuMatrix)):
+        for R in range(0,int(TrainingLen)):
+            PHI[R][C] = GetRadialBasisOut(DataT[R], MuMatrix[C], BigSigInv)
+    #print ("PHI Generated..")
+    return PHI
+
+# In this function using the so generated phi value we are performing Moore-Penrose Inverse multiplied with the
+# target label to obtaing the weights for the closed from.
+def GetWeightsClosedForm(PHI, T, Lambda):
+    Lambda_I = np.identity(len(PHI[0]))
+    for i in range(0,len(PHI[0])):
+        Lambda_I[i][i] = Lambda
+    PHI_T       = np.transpose(PHI)
+    PHI_SQR     = np.dot(PHI_T,PHI)
+    PHI_SQR_LI  = np.add(Lambda_I,PHI_SQR)
+    PHI_SQR_INV = np.linalg.inv(PHI_SQR_LI)
+    INTER       = np.dot(PHI_SQR_INV, PHI_T)
+    W           = np.dot(INTER, T)
+    ##print ("Training Weights Generated..")
+    return W
+
+def GetPhiMatrix(Data, MuMatrix, BigSigma, TrainingPercent = 80):
+    DataT = np.transpose(Data)
+    TrainingLen = math.ceil(len(DataT)*(TrainingPercent*0.01))         
+    PHI = np.zeros((int(TrainingLen),len(MuMatrix))) 
+    BigSigInv = np.linalg.inv(BigSigma)
+    for  C in range(0,len(MuMatrix)):
+        for R in range(0,int(TrainingLen)):
+            PHI[R][C] = GetRadialBasisOut(DataT[R], MuMatrix[C], BigSigInv)
+    #print ("PHI Generated..")
+    return PHI
+
+#Using the above generated weights we are doing validation testing.
+def GetValTest(VAL_PHI,W):
+    Y = np.dot(W,np.transpose(VAL_PHI))
+    ##print ("Test Out Generated..")
+    return Y
+
+# Root mean squared error is performed from the predicted target label and the orginal target label and thereby
+# the accuracy is calculated.
+def GetErms(VAL_TEST_OUT,ValDataAct):
+    sum = 0.0
+    t=0
+    accuracy = 0.0
+    counter = 0
+    val = 0.0
+    for i in range (0,len(VAL_TEST_OUT)):
+        sum = sum + math.pow((ValDataAct[i] - VAL_TEST_OUT[i]),2)
+        if(int(np.around(VAL_TEST_OUT[i], 0)) == ValDataAct[i]):
+            counter+=1
+    accuracy = (float((counter*100))/float(len(VAL_TEST_OUT)))
+    #print ("Accuracy Generated..")
+    #print ("Validation E_RMS : " + str(math.sqrt(sum/len(VAL_TEST_OUT))))
+    return (str(accuracy) + ',' +  str(math.sqrt(sum/len(VAL_TEST_OUT))))
+
+
+# ## Fetch and Prepare Dataset
+
+# In[262]:
+
+
+# Raw data and target values are fetched from the csv files.
+RawTarget = GetTargetVector('Querylevelnorm_t.csv')
+RawData   = GenerateRawData('Querylevelnorm_X.csv',IsSynthetic)
+
+
+# ## Prepare Training Data
+
+# In[263]:
+
+
+TrainingTarget = np.array(GenerateTrainingTarget(RawTarget,TrainingPercent))
+TrainingData   = GenerateTrainingDataMatrix(RawData,TrainingPercent)
+print(TrainingTarget.shape)
+print(TrainingData.shape)
+
+
+# ## Prepare Validation Data
+
+# In[264]:
+
+
+ValDataAct = np.array(GenerateValTargetVector(RawTarget,ValidationPercent, (len(TrainingTarget))))
+ValData    = GenerateValData(RawData,ValidationPercent, (len(TrainingTarget)))
+print(ValDataAct.shape)
+print(ValData.shape)
+
+
+# ## Prepare Test Data
+
+# In[265]:
+
+
+TestDataAct = np.array(GenerateValTargetVector(RawTarget,TestPercent, (len(TrainingTarget)+len(ValDataAct))))
+TestData = GenerateValData(RawData,TestPercent, (len(TrainingTarget)+len(ValDataAct)))
+print(ValDataAct.shape)
+print(ValData.shape)
+
+
+# ## Closed Form Solution [Finding Weights using Moore- Penrose pseudo- Inverse Matrix]
+
+# In[266]:
+
+
+ErmsArr = []
+AccuracyArr = []
+# K-means clustering is performed to obtaing 10 mean values for phi design matrix calculation.
+# Here we have 10 clusters and the centroid of each of these clusters are obtained.
+kmeans = KMeans(n_clusters=M, random_state=0).fit(np.transpose(TrainingData))
+Mu = kmeans.cluster_centers_
+
+BigSigma     = GenerateBigSigma(RawData, Mu, TrainingPercent,IsSynthetic)
+TRAINING_PHI = GetPhiMatrix(RawData, Mu, BigSigma, TrainingPercent)
+W            = GetWeightsClosedForm(TRAINING_PHI,TrainingTarget,(C_Lambda)) 
+TEST_PHI     = GetPhiMatrix(TestData, Mu, BigSigma, 100) 
+VAL_PHI      = GetPhiMatrix(ValData, Mu, BigSigma, 100)
+
+
+# In[267]:
+
+
+print(Mu.shape)
+print(BigSigma.shape)
+print(TRAINING_PHI.shape)
+print(W.shape)
+print(VAL_PHI.shape)
+print(TEST_PHI.shape)
+
+
+# ## Finding Erms on training, validation and test set 
+
+# In[268]:
+
+
+TR_TEST_OUT  = GetValTest(TRAINING_PHI,W)
+VAL_TEST_OUT = GetValTest(VAL_PHI,W)
+TEST_OUT     = GetValTest(TEST_PHI,W)
+
+TrainingAccuracy   = str(GetErms(TR_TEST_OUT,TrainingTarget))
+ValidationAccuracy = str(GetErms(VAL_TEST_OUT,ValDataAct))
+TestAccuracy       = str(GetErms(TEST_OUT,TestDataAct))
+print(TrainingAccuracy)
+
+print(ValidationAccuracy)
+
+print(TestAccuracy)
+
+
+# In[269]:
+
+
+print ('UBITname      = venktesh')
+print ('Person Number = 50289400')
+print ('----------------------------------------------------')
+print ("------------------LeToR Data------------------------")
+print ('----------------------------------------------------')
+print ("-------Closed Form with Radial Basis Function-------")
+print ('----------------------------------------------------')
+print ("M = 10 \nLambda = 0.9")
+print ("E_rms Training   = " + str(float(TrainingAccuracy.split(',')[1])))
+print ("E_rms Validation = " + str(float(ValidationAccuracy.split(',')[1])))
+print ("E_rms Testing    = " + str(float(TestAccuracy.split(',')[1])))
+
+
+# ## Gradient Descent solution for Linear Regression
+
+# In[17]:
+
+
+print ('----------------------------------------------------')
+print ('--------------Please Wait for 2 mins!----------------')
+print ('----------------------------------------------------')
+
+
+# In[18]:
+
+
+# Here in SGD we are randomnly initialzing the weights by multiplying with a constant here it is 220
+W_Now        = np.dot(220, W)
+La           = 2
+# Learning rate is 0.01 that is the amount of steps taken towards reaching the minimum.
+learningRate = 0.01
+L_Erms_Val   = []
+L_Erms_TR    = []
+L_Erms_Test  = []
+W_Mat        = []
+
+# In SGD we update the weights by going thorugh each of the data point and then we calucalte the Erms for training
+# validaton and testing data.
+for i in range(0,400):
+    
+    #print ('---------Iteration: ' + str(i) + '--------------')
+    Delta_E_D     = -np.dot((TrainingTarget[i] - np.dot(np.transpose(W_Now),TRAINING_PHI[i])),TRAINING_PHI[i])
+    La_Delta_E_W  = np.dot(La,W_Now)
+    Delta_E       = np.add(Delta_E_D,La_Delta_E_W)    
+    Delta_W       = -np.dot(learningRate,Delta_E)
+    W_T_Next      = W_Now + Delta_W
+    W_Now         = W_T_Next
+    
+    #-----------------TrainingData Accuracy---------------------#
+    TR_TEST_OUT   = GetValTest(TRAINING_PHI,W_T_Next) 
+    Erms_TR       = GetErms(TR_TEST_OUT,TrainingTarget)
+    L_Erms_TR.append(float(Erms_TR.split(',')[1]))
+    
+    #-----------------ValidationData Accuracy---------------------#
+    VAL_TEST_OUT  = GetValTest(VAL_PHI,W_T_Next) 
+    Erms_Val      = GetErms(VAL_TEST_OUT,ValDataAct)
+    L_Erms_Val.append(float(Erms_Val.split(',')[1]))
+    
+    #-----------------TestingData Accuracy---------------------#
+    TEST_OUT      = GetValTest(TEST_PHI,W_T_Next) 
+    Erms_Test = GetErms(TEST_OUT,TestDataAct)
+    L_Erms_Test.append(float(Erms_Test.split(',')[1]))
+
+
+# In[19]:
+
+
+print ('----------Gradient Descent Solution--------------------')
+print ("M = 15 \nLambda  = 0.0001\neta=0.01")
+print ("E_rms Training   = " + str(np.around(min(L_Erms_TR),5)))
+print ("E_rms Validation = " + str(np.around(min(L_Erms_Val),5)))
+print ("E_rms Testing    = " + str(np.around(min(L_Erms_Test),5)))
+
